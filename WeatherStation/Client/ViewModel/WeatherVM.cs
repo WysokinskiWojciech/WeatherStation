6c1205e0c0b4d9b5bd10709b35ca4e3f1c083936
WysokinskiWojciech <79663553+WysokinskiWojciech@users.noreply.github.com>
2022-12-30 13:17:56 +0100

    Add weather view model.

---
+++ WeatherStation/Client/ViewModel/WeatherVM.cs
@@ -0,0 +1,18 @@
+﻿using WeatherStation.Shared;
+
+namespace WeatherStation.Client.ViewModel
+{
+    public class WeatherVM
+    {
+        public double Temperature { get; set; }
+        public double Pressure { get; set; }
+        public static WeatherVM Create(Weather weather)
+        {
+            return new WeatherVM()
+            {
+                Pressure = weather.Pressure,
+                Temperature = weather.Temperature
+            };
+        }
+    }
+}
